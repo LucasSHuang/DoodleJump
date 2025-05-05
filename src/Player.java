cc57ebe4c1394bdbee7987af8bf4f8ff381b68e5
Lucas Huang <lucas.huang@menloschool.org>
2025-05-05 13:26:26 -0700

    Finished comments

--- src/Player.java
+++ src/Player.java
@@ -18,6 +18,7 @@ public class Player {
     private int dx;
     private double dy;
 
+    // Constructor
     public Player(GameViewer window) {
         this.window = window;
         this.right = new ImageIcon("Resources/right.png").getImage();
@@ -28,6 +29,7 @@ public class Player {
         this.dy = 0;
     }
 
+    // Getters and setters
     public int getX() {
         return x;
     }
@@ -45,7 +47,9 @@ public class Player {
     }
 
     public void setX() {
+        // Changes the x value by dx
         x += dx;
+        // Makes it so that if the player goes off the screen it wraps around to the opposite side
         if (x > GameViewer.WINDOW_WIDTH - PLAYER_SIZE) {
             x = 0;
         }
@@ -54,6 +58,7 @@ public class Player {
         }
     }
 
+    // Makes the player fall(or rise) at the speed of dx until a certain speed is reached
     public void fall() {
         y += dy;
         dy += GRAVITY;
@@ -62,32 +67,45 @@ public class Player {
         }
     }
 
+    // Checks to see if the player is touching a platform
     public boolean touchingPlatform(Platform p) {
+        // Gets the platform x and y coordinates
         int px = p.getX();
         int py = p.getY();
+        // Makes sure the player is within the x parameters of the platform
         boolean rightX = x + PLAYER_SIZE >= px && x <= px + Platform.PLATFORM_WIDTH;
+        // Makes sure the player is within the y parameters of the platform
         boolean rightY = y + PLAYER_SIZE >= py && y <= py + Platform.PLATFORM_HEIGHT;
+        // Makes sure the player is falling
         boolean falling = dy == GRAVITY_LIMIT;
         return rightX && rightY && falling;
     }
 
+    // Moves the player
     public void move(ArrayList<Platform> platforms) {
         setX();
         fall();
+        // Checks if the player is touching for all platforms
         for (Platform p : platforms) {
+            // If touching a platform the player jumps
             if (touchingPlatform(p)) {
                 jump();
             }
         }
     }
+
+    // Makes the player jump
     public void jump() {
         dy = -30;
     }
 
+    // Draws the player
     public void draw(Graphics g) {
+        // Makes it so that if the player is moving right the player faces right
         if (dx >= 0) {
             g.drawImage(right, x, y, PLAYER_SIZE, PLAYER_SIZE, window);
         }
+        // If the player is moving left it faces left
         else {
             g.drawImage(left, x, y, PLAYER_SIZE, PLAYER_SIZE, window);
         }
